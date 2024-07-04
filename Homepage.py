911741f01a249f8dd6d1b21fc98ed15fdbc1fd82
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2024-07-04 16:04:16 -0600

    a

--- Homepage.py
+++ Homepage.py
@@ -104,7 +104,7 @@ def vote():
         submitted = st.form_submit_button("Submit form")
 
     if submitted:
-        #st.write(barcode)
+        st.write(barcode)
         st.write(st.session_state.barcode)
 
 
