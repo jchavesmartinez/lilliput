4862e43f5a2f33d1676248b28eaa26ee27561da5
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2024-07-04 17:45:02 -0600

    a

--- Homepage.py
+++ Homepage.py
@@ -100,7 +100,8 @@ def vote(master_data_dict):
 
         # Iterate over variables and create a text input for each
         for i in variables:
-            responses[i["variable_name"]] = st.text_input(i["variable_name"], 0 , key=i["variable_name"])
+            if i['variable_type']=='Independant':
+                responses[i["variable_name"]] = st.text_input(i["variable_name"], 0 , key=i["variable_name"])
         
                
         submitted = st.form_submit_button("Submit form", use_container_width=True)
