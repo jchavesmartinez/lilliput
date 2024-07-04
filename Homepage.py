df3ab7484870e44034891a4f58046234675e4a65
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2024-07-04 12:29:13 -0600

    a

--- Homepage.py
+++ Homepage.py
@@ -97,8 +97,5 @@ if "vote" not in st.session_state:
     st.write("Vote for your favorite")
     if st.button("A", use_container_width=True):
         vote("A")
-    if st.button("B"):
-        vote("B")
-else:
-    f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
+
 
