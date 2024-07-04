fe43bffa72f0ed667fcf7cb3cc27172c130777fb
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2024-07-04 15:09:18 -0600

    a

--- Homepage.py
+++ Homepage.py
@@ -75,7 +75,7 @@ def generate_barcode(number):
     number = str(number).zfill(12)
     barcode = EAN13(number, writer=ImageWriter())
     barcode.save("barcode")
-    st.write(number)
+    st.write(barcode)
     return "barcode.png"
 
 @st.experimental_dialog("New experiment")
