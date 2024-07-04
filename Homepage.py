599b7744968bf12951d469a219c45df4c9d907e7
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2024-07-04 13:11:37 -0600

    a

--- Homepage.py
+++ Homepage.py
@@ -89,10 +89,19 @@ st.markdown(description)
 st.markdown("---")
 
 
+
+
 @st.experimental_dialog("Cast your vote")
 def vote(item):
-    st.write(f"Why is {item} your favorite?")
-    reason = st.text_input("Because...")
+
+    vairabless = read_file_googledrive(credentials,file_id)
+
+    for i in variables:
+        st.write(i)
+
+
+
+
     if st.button("Submit"):
         st.session_state.vote = {"item": item, "reason": reason}
         st.rerun()
