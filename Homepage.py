ab5504343b59bfd4f276ddedeb61614668f4464c
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2024-07-04 15:48:34 -0600

    a

--- Homepage.py
+++ Homepage.py
@@ -99,10 +99,11 @@ def vote():
         for i in variables:
             responses[i["variable_name"]] = st.text_input(i["variable_name"],0 , key=i["variable_name"])
         
+        responses['barcode']=barcode
+        
         submitted = st.form_submit_button("Submit form")
 
     if submitted:
-        responses['barcode']=barcode
         st.write(responses)
 
 
