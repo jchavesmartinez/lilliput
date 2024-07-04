fef47753170e40d438c0292f97a8f341cf594b28
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2024-07-04 16:06:24 -0600

    a

--- Homepage.py
+++ Homepage.py
@@ -91,9 +91,7 @@ def vote():
 
         if 'barcode' not in st.session_state:
             st.session_state['barcode'] = barcode
-        
-        else:
-            st.session_state['barcode'] = barcode
+
         
         variables = read_file_googledrive(credentials,'1k-Gnh-xUFUXej14D6ABMhGeGe8dXGxyT')
 
