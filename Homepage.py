9c5610373cbac5281f61759177cbb8dbfa59d810
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2024-07-04 18:05:19 -0600

    A

--- Homepage.py
+++ Homepage.py
@@ -232,7 +232,7 @@ def vote(item):
     st.write(f"Why is {item} your favorite?")
     reason = st.text_input("Because...")
     if st.button("Submit"):
-        st.session_state.vote = {"item": item, "reason": reason}
+        #st.session_state.vote = {"item": item, "reason": reason}
         st.rerun()
 
 if "vote" not in st.session_state:
