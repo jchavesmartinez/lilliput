55d14fc40fd052e72946cfd10ffe644fec95932c
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2024-07-04 18:07:11 -0600

    a

--- Homepage.py
+++ Homepage.py
@@ -227,23 +227,4 @@ master_data_df_edit = st.data_editor(master_data_df)
 
 
 
-@st.experimental_dialog("Cast your vote")
-def vote(item):
-    st.write(f"Why is {item} your favorite?")
-    reason = st.text_input("Because...")
-    if st.button("Submit"):
-        st.session_state.vote = {"item": item, "reason": reason}
-        #st.rerun()
-        print("wenas")
-
-if "vote" not in st.session_state:
-    st.write("Vote for your favorite")
-    if st.button("A"):
-        vote("A")
-    if st.button("B"):
-        vote("B")
-else:
-    f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
-
-
 
