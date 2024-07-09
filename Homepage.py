9312973be32d1e739b9fb4984778015b3b13c8fd
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2024-07-09 15:06:23 -0600

    a

--- Homepage.py
+++ Homepage.py
@@ -197,7 +197,7 @@ def complete_value(master_data_dict):
         barcode = str(barcode).split("'")[0]
         image = Image.open(barcode_path)
 
-        st.image(image, caption='Generated Barcode')
+        st.image(image, caption='Generated Barcode', use_container_width=True)
 
 
     else:
