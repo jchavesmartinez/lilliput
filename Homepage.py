6821948df0f8bda0f827497fbcfe97994360e172
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2024-07-11 16:23:28 -0600

    a

--- Homepage.py
+++ Homepage.py
@@ -357,6 +357,12 @@ if search_barcode:
 else:
     filtered_df = master_data_df
 
+cols_to_move = ['barcode', 'file']
+
+# Reorder the columns
+cols = cols_to_move + [col for col in df.columns if col not in cols_to_move]
+filtered_df = filtered_df[cols]
+
 master_data_df_edit = st.data_editor(filtered_df, num_rows="dynamic", hide_index=True, use_container_width=True,
     column_config={
         "file": st.column_config.LinkColumn(
