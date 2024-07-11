719cafd61eea5bf1be4c364dd9efd7b61d9c9ca9
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2024-07-11 15:24:56 -0600

    a

--- Homepage.py
+++ Homepage.py
@@ -119,7 +119,7 @@ def generate_barcode(number):
 
     pdf_buffer = io.BytesIO()
     c = canvas.Canvas(pdf_buffer, pagesize=letter)
-    c.drawImage(barcode_filename, 50, 0, width=523, height=280)  # Adjust the position and size as needed
+    c.drawImage(barcode_filename, 50, 0, width=523, height=100)  # Adjust the position and size as needed
     c.showPage()
     c.save()
     
