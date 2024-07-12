080e4a5f1ae5bee3eaff8f40a5de5de2c0ec5808
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2024-07-11 21:01:52 -0600

    a

--- Homepage.py
+++ Homepage.py
@@ -363,7 +363,7 @@ if search_barcode:
 
         with col1d:
             delete_y = st.button("Yes", use_container_width=True, type="primary", key="yes_delete")
-            master_data_dict = [entry for entry in data if entry['barcode'] != str(search_barcode)]
+            master_data_dict = [entry for entry in master_data_dict if entry['barcode'] != str(search_barcode)]
             st.write(master_data_dict)
         
         with col2d:
