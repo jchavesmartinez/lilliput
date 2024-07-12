b649e2c67d2393a1e69755bff8867561fca6e948
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2024-07-11 21:19:24 -0600

    a

--- Homepage.py
+++ Homepage.py
@@ -283,6 +283,14 @@ def complete_value(master_data_dict):
     else:
         st.write("No value typed")
 
+@st.experimental_dialog("Update experiment", width="large")
+def delete_value(master_data_dict):
+
+    with st.form("my_form", clear_on_submit=False):
+    
+        delete=st.form_submit_button("delete")
+        do_not_delete=st.form_submit_button("do not delete")
+
 
 #---------------------------- Codigo general --------------------------------
 
@@ -354,32 +362,14 @@ if st.button("Update a value (dependent variables)", use_container_width=True):
 
 search_barcode = st.text_input("Search by barcode")
 
-if 'delete_barcode' not in st.session_state:
-    st.session_state['delete_barcode'] = 'No'
-
-st.write(st.session_state.delete_barcode)    
 
 # Filter the dataframe based on the input
 if search_barcode:
     filtered_df = master_data_df[master_data_df['barcode'].str.contains(search_barcode)]
     barcode_delete= st.button("Delete entry: "+str(search_barcode), use_container_width=True, type="primary")
     if barcode_delete:
-        st.session_state['delete_barcode'] = 'Yes'
+        delete_value(master_data_dict)
 
-    if st.session_state['delete_barcode'] == 'Yes':
-
-        col1d, col2d = st.columns(2)
-
-        with col1d:
-            delete_y = st.button("Yes", use_container_width=True, type="primary", key="yes_delete")
-            if delete_y:
-                master_data_dict = [entry for entry in master_data_dict if entry['barcode'] != str(search_barcode)]
-                st.write(master_data_dict)
-        
-        with col2d:
-            delete_n = st.button("No", use_container_width=True, type="secondary", key="no_delete")
-            if delete_n:
-                st.session_state['delete_barcode'] = 'No'
     
 else:
     filtered_df = master_data_df
