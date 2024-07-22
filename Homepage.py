5387c4fbc2f963ac173e2b19d6613faf7e499411
jchavesmartinez <110587345+jchavesmartinez@users.noreply.github.com>
2024-07-22 13:30:19 -0600

    a

--- Homepage.py
+++ Homepage.py
@@ -168,8 +168,8 @@ def new_value(master_data_dict):
             # Iterate over variables and create a text input for each
             for i in variables:
                 if i['variable_type']=='Independant': # and synthesis_methods in i["synthesis_methods"]:
-                    st.write(i['synthesis_methods'])
-                    responses[i["variable_name"]] = st.text_input(i["variable_name"], "" , key=i["variable_name"])
+                    if synthesis_methods in i['synthesis_methods']:
+                        responses[i["variable_name"]] = st.text_input(i["variable_name"], "" , key=i["variable_name"])
 
             purification_methods = st.selectbox(
                 "Purification Methods",
